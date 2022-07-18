3efee7979e546e06a7c1a7bc2e5744eadc10b960
arjund-06 <davearjun25@gmail.com>
2022-07-18 15:05:28 +0530

    feat(dev): Initialize repo
    Signed-off-by: arjund-06 <davearjun25@gmail.com>

---
+++ functions/apis/users.js
@@ -0,0 +1,49 @@
+const { connection } = require('../application')
+
+exports.createUser = function(req, res) {
+    uid = "ljnjdsgj"
+    const newUser = {
+        name: req.body.name,
+        id: uid,
+        orgId: req.body.orgId,
+        email: req.body.email,
+    };
+    connection.query(`INSERT INTO users SET ?`, newUser, function(error, results, fields) {
+        if (error) throw error;
+        r = results;
+        res.send({ r: r, data: newUser });
+        return;
+    });
+}
+
+exports.getAllUsers = function(req, res) {
+    let r;
+    connection.query('SELECT * FROM users', function(error, results, fields) {
+        if (error) throw error;
+        r = results;
+        // res.send(`The solution is: ${r} feilds, ${fields}`);
+        // console.log("R", r);
+        res.send(r);
+        return;
+    });
+}
+
+exports.getuserByUserId = function(req, res) {
+    let r;
+    connection.query(`SELECT * FROM users where id = '${req.params.id}'`, function(error, results, fields) {
+        if (error) throw error;
+        r = results;
+        res.send(r);
+        return;
+    });
+}
+
+exports.getUserByOrgId = function(req, res) {
+    let r;
+    connection.query(`SELECT * FROM users where orgId = '${req.params.orgId}'`, function(error, results, fields) {
+        if (error) throw error;
+        r = results;
+        res.send(r);
+        return;
+    });
+}
---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const { connection } = require('./application')
+
+const { createUser, getuserByUserId, getUserByOrgId, getAllUsers } = require("./apis/users");
+
+app.use(express.json())
+
+connection.connect()
+
+app.get('/users', (req, res) => { getAllUsers(req, res) });
+app.post('/users', (req, res) => { createUser(req, res) });
+app.get('/users/:id', (req, res) => { getuserByUserId(req, res) });
+app.get('/users/org/:orgId', (req, res) => { getUserByOrgId(req, res) });
+
+
+app.listen(3000, () => console.log("Running"));
