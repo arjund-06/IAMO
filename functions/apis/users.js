cb4dd3c1ea4707e6e94562ab1d604d11ffd5111a
vaishnavityra712 <vaishnavityra712@gmail.com>
2022-08-23 11:05:43 +0530

    feat(dev):Made component api

--- functions/apis/users.js
+++ functions/apis/users.js
@@ -47,3 +47,4 @@ exports.getUserByOrgId = function(req, res) {
         return;
     });
 }
+
--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const app = express();
 const { connection } = require('./application')
 
 const { createUser, getuserByUserId, getUserByOrgId, getAllUsers } = require("./apis/users");
-
+const { createComponent,getAllComponents, getComponentById } = require("./apis/original_components");
 app.use(express.json())
 
 connection.connect()
@@ -12,6 +12,10 @@ app.get('/users', (req, res) => { getAllUsers(req, res) });
 app.post('/users', (req, res) => { createUser(req, res) });
 app.get('/users/:id', (req, res) => { getuserByUserId(req, res) });
 app.get('/users/org/:orgId', (req, res) => { getUserByOrgId(req, res) });
+// app.get('/original_components/:id[p]',(req,res)=>{ getComponentById(req,res)});
 
+app.get('/original_components',(req,res)=>{ getAllComponents(req,res) });
+app.post('/original_components', (req, res) => { createComponent(req, res) });
+app.get('/original_components/:id',(req,res)=>{ getComponentById(req,res) });
 
 app.listen(3000, () => console.log("Running"));
